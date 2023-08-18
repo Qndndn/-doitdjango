7736926334de773588c068a88d4be1da59c2c2d3
권효진 <qndndn0510@gmail.com>
2023-08-18 20:44:04 +0900

    보드 성공

---
+++ board/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('create/',views.BoardCreate.as_view()),
+    path('<int:pk>/', views.BoardDetail.as_view()),
+    path('', views.BoardList.as_view()),
+]
